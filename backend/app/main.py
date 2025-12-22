a2fa98cd4b01964ac4ac291c6476b94ba4103e21
Gms006 <gptjoao01@gmail.com>
2025-12-22 16:11:32 -0300

    Add logout action to user badge

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,10 +12,8 @@ app = FastAPI(title="CertHub API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
+        "http://localhost:8011",
+        "http://127.0.0.1:8011",
     ],
     allow_credentials=True,
     allow_methods=["*"],
