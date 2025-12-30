1b893c20016a696828fd944eb48de13556c04b99
Gms006 <gptjoao01@gmail.com>
2025-12-30 17:12:10 -0300

    Fix migration and minor changes.

--- backend/alembic/versions/0010_device_auto_approve.py
+++ backend/alembic/versions/0010_device_auto_approve.py
@@ -1,7 +1,7 @@
 """add device auto_approve flag.
 
 Revision ID: 0010_device_auto_approve
-Revises: 0009_job_payload_tokens
+Revises: 0010_merge_0009_heads
 Create Date: 2025-02-14 00:00:00.000000
 """
 
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "0010_device_auto_approve"
-down_revision = ("0009_job_payload_tokens", "0009_agent_mvp_fields")
+down_revision = "0010_merge_0009_heads"
 branch_labels = None
 depends_on = None
 
